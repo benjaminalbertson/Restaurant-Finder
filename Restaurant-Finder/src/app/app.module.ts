35addaf19939e40fc6603df5f4e6dff7e63e295f
Benjamin Albertson <benalbertson1@hotmail.com>
2019-04-10 11:29:10 -0500

    added components

--- Restaurant-Finder/src/app/app.module.ts
+++ Restaurant-Finder/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ListComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
