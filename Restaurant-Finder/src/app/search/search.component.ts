9505aef494b6f131452c152c8aba91f58ce3648c
Benjamin Albertson <benalbertson1@hotmail.com>
2019-04-10 13:15:00 -0500

    worked on searchcomponent

--- Restaurant-Finder/src/app/search/search.component.ts
+++ Restaurant-Finder/src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestaurantService } from '../restaurant.service'
+import { Restaurant } from '../restaurant'
 
 @Component({
   selector: 'app-search',
@@ -10,7 +11,45 @@ export class SearchComponent implements OnInit {
 
   constructor(private resService: RestaurantService) { }
 
+  searchTags: string[];
+  restaurants: Restaurant[] = this.resService.getRestaurants();
+
   ngOnInit() {
+  }
+
+  searchRestaurants(): Restaurant[]{
+    var i: number;
+    var j: number;
+    var k: number;
+
+    for(i = 0; i < this.searchTags.length; i++)
+    {
+      for(j = 0; j < this.restaurants.length; j++)
+      {
+        for(k = 0; k < this.restaurants[j].tags.length; k++)
+        {
+          if(this.restaurants[j].tags[k] != this.searchTags[i])
+          {
+            
+          }
+          else{
+
+          }
+        }
+      }
+    }
+
+
+  }
+
+  addTag(tag: string){
+    this.searchTags.push(tag);
+  }
+
+  clear(){
+    while(this.searchTags.length != 0){
+      this.searchTags.pop;
+    }
   }
 
 }
